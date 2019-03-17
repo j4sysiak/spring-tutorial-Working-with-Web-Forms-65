f97475d30d707db1a274f5e097eeb2765c7c6475
j4sysiak <j4sysiak@gmail.com>
2019-03-17 19:53:25 +0100

    1

---
+++ spring-tutorial-Working-with-Web-Forms-65/src/com/jaceksysiak/spring/web/controllers/OffersController.java
@@ -0,0 +1,53 @@
+package com.jaceksysiak.spring.web.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.jaceksysiak.spring.web.dao.Offer;
+import com.jaceksysiak.spring.web.service.OffersService;
+
+@Controller
+public class OffersController {
+	
+	private OffersService offersService;
+	
+	@Autowired
+	public void setOffersService(OffersService offersService) {
+		this.offersService = offersService;
+	}
+	
+	@RequestMapping(value=("/test"), method=RequestMethod.GET)
+	public String showTest(Model model, @RequestParam("id") String id) {
+		
+		System.out.println("id: " + id);
+		return "home";
+	}
+
+	@RequestMapping("/offers")
+	public String showOffers(Model model) {
+		
+		List<Offer> offers = offersService.getCurrent();
+		
+		model.addAttribute("offers", offers);
+		
+		return "offers";
+	}
+	
+	@RequestMapping("/createoffer")
+	public String createOffer() {
+	
+		return "createoffer";
+	}
+	
+	@RequestMapping("/docreate")
+	public String doCreate() {
+	
+		return "offercreated";
+	}
+}
